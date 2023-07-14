555cd4d45170da1217d1cf7bf48fbb2f2b3b52dd
ocimar84 <79640465+ocimar84@users.noreply.github.com>
2023-07-14 14:57:29 +0000

    to close

--- store/views.py
+++ store/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 def products(request):
-    return render(request, 'myapp/templates/product.html')
+    return render(request, 'myapp/templates/product')
