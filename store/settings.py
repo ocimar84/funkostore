8b979500e36b81648d30d108c41ccb57afe7883a
ocimar84 <ocimarcostaf@gmail.com>
2023-07-24 20:44:28 +0000

    fix admin and server

--- store/settings.py
+++ store/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w1ujp$pvdg0d)zuebtx0idr5%uuk&q!bc*y*_mx#@a=nz2w9h@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -135,7 +135,7 @@ USE_X_FORWARDED_HOST = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
